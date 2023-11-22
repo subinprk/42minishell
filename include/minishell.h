c50476bd5298e3f6830e2b62963acf246320f9de
Irene Rivero Casal <irivero-@c4a6c3.42berlin.de>
2023-11-22 16:23:17 +0100

    added the struct lists

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: subpark <subpark@student.42.fr>            +#+  +:+       +#+        */
+/*   By: irivero- <irivero-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 15:24:03 by subpark           #+#    #+#             */
-/*   Updated: 2023/11/22 15:26:49 by subpark          ###   ########.fr       */
+/*   Updated: 2023/11/22 16:23:02 by irivero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,25 @@
 # include <stdio.h>
 # include <sys/wait.h>
 # include <errno.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
+typedef struct s_cmd 
+{
+    char **cmdline;
+    int flag;
+    char quote;
+} t_cmd;
+
+typedef struct s_data
+{
+    t_list  *lstlast;
+    t_cmd   *cmd;
+    char    *buf;
+}	t_data;
 
 #endif
