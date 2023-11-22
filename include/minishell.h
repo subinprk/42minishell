ec1c3235ea1e69fa5f8d9d80122af8263e0551a2
Subin Park <subpark@c4a6c4.42berlin.de>
2023-11-22 15:31:12 +0100

    header, a function for envpath

---
+++ include/minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: subpark <subpark@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/22 15:24:03 by subpark           #+#    #+#             */
+/*   Updated: 2023/11/22 15:26:49 by subpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <sys/wait.h>
+# include <errno.h>
+
+#endif
