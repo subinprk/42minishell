8169a48a8d2376d68c6cea0de1440dd5156a0477
Irene Rivero Casal <irivero-@c4a6c3.42berlin.de>
2023-11-23 14:12:35 +0100

    some changes in how the prompt might go

--- src/generate_prompt.c
+++ src/generate_prompt.c
@@ -6,31 +6,102 @@
 /*   By: irivero- <irivero-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 16:30:36 by irivero-          #+#    #+#             */
-/*   Updated: 2023/11/22 18:27:17 by irivero-         ###   ########.fr       */
+/*   Updated: 2023/11/23 14:09:38 by irivero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-void	generate_prompt(t_data *data, char **envp)
+char	**g_envp = NULL;
+int		g_exit_status = 0;
+
+/* generates and prints a command prompt to the standard output*/
+void	generate_prompt(void)
 {
-	var_init(data, 1, envp);
-	data->d_null = open("/dev/null", O_WRONLY);
-	data->prompt = readline("our shell");
-	if (!data->prompt)
-		exit_minishell(data);
-	add_history(data->prompt);
-	if(to_array(data))
-		exec_minishell(data);
-	free(data->args_size);
+	static char	*current_path;
+
+	ft_putstr_fd("\033[1;32m", 1);
+	ft_putstr_fd("our shell: ", 1);
+	ft_putstr_fd("\033[0m", 1);
+	ft_putstr_fd("\033[1;34m", 1);
+	ft_putstr_fd(getcwd(current_path, 1024), 0);
+	ft_putstr_fd("\033[0m", 1);
+	ft_putstr_fd("$ ", 1);
 }
 
-void	var_init(t_data *data, int argc, char **envp)
+/* reads a line from standard input and appends it to the content of line
+it uses a loop to keep reading util the end of input.
+it returns the value of ret.*/
+int		get_line(char **line)
 {
+	int		ret;
+	char	*buf;
+	char	*tmp;
 
+	*line = ft_strdup("");
+	while (1)
+	{
+		ret = read_from_stdin(&buf);
+		tmp = ft_strjoin(*line, buf);
+		free(*line);
+		free(buf);
+		*line = tmp;
+		if (ret == 0)
+		{
+			ft_putstr_fd("exit\n", 1);
+			if (ft_strlen(*line) != 0)
+				continue;
+			ft_putstr_fd("exit\n", 1);
+		}	
+		break ;
+	}
+	return (ret);
 }
 
+/* reads from standard input into the buffer*/
+int		read_from_stdin(char **buf)
+{
+	int 	bytes_read;
+	size_t	size;
+
+	size = 1024;
+	bytes_read = 0;
+	*buf = ft_strnew(size);
+	if (!buf)
+		return (-1);
+	bytes_read = read(0, *buf, size);
+	if (bytes_read == 0)
+	{
+		free(*buf);
+		return (-1);
+	}
+	else if (bytes_read < 0)
+	{
+		free(*buf);
+		return (-1);
+	}
+	return (bytes_read);
+}
+/*
 void	exec_minishell(t_data *data)
 {
+	//check if command is a redirection or a pipe
+	//if not, printf error
+
+	//else loop
+	//end loop
+
+}
+*/
+int		main(int argc, char **argv, char **envp)
+{
+	int		ret;
 	
+	ret = 1;
+	while (ret)
+	{
+		generate_prompt();
+
+	}
+	return (0);
 }
--- src/parser.c
+++ src/parser.c
@@ -6,41 +6,34 @@
 /*   By: irivero- <irivero-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 15:32:13 by irivero-          #+#    #+#             */
-/*   Updated: 2023/11/22 18:26:16 by irivero-         ###   ########.fr       */
+/*   Updated: 2023/11/23 14:06:49 by irivero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int to_array(t_data *data)
+/* determine if a given string, consistes of whitespace characters*/
+int        is_whitespace(char *c)
 {
-    //to do a string_count function!
-    data->current_index = 0;
-    data->args_index = 0;
-    data->args_count = -1;
-    data->quote = 0;
+    int        i;
+    int        space;
 
-    if (!data->args_size[0] || data->prompt[0] == '\n' || !data->prompt[0])
+    if(c[0] == 0)
+        return (1);
+    while (c[i])
     {
-        free(data->prompt);
-        return (0);
+        if (c[i] == '\r' || c[i] == '\v' || c[i] == '\t' || c[i] == '\f')
+            return (1);
+        else if (c[i] == ' ')
+            space++;
+        i++;
     }
-    while (data->args_size[++data->args_count])
-        ;
-    data->args = ft_calloc(data->args_count + 1, sizeof(char *));
-    if (!data->args)
-        return (0);
-    while (--data->args_count >= 0)
-        data->args[data->args_count] = ft_calloc(data->args_size[data->args_count] + 1,
-            sizeof(char));
-    while (data->prompt[data->current_index] == ' ' || 
-        data->prompt[data->current_index] == '\t')
-        data->current_index++;
-    while (data->prompt[data->current_index] && data->prompt[data->current_index]
-            != '\n')
-        //parsing function
-    data->args[data->args_index][++data->args_count] = '\0';
-    data->args[data->args_index + 1] = 0;
-    free(data->prompt);
-    return (1);
+    if (space == (int)ft_strlen(c))
+        return (1);
+    return (0);
+}
+
+void    *parse(char *input)
+{
+    
 }
