28be8541e3ed5960deee1a27d4d494c567264685
Subin Park <subpark@c4a10c8.42berlin.de>
2023-11-30 20:18:56 +0100

    made redirect pipes, have to think about when to remove temporary file for heredoc

--- src/fork/redirect_pipe_tools.c
+++ src/fork/redirect_pipe_tools.c
@@ -6,7 +6,7 @@
 /*   By: subpark <subpark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 15:41:10 by subpark           #+#    #+#             */
-/*   Updated: 2023/11/30 15:33:08 by subpark          ###   ########.fr       */
+/*   Updated: 2023/11/30 20:17:36 by subpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	close_two_exit(int *fd1, int *fd2)
 	exit(errno);
 }
 
-void	re_type_l_pipes(int *pipefd, int filefd)
+void	re_type_r_pipes(int *pipefd, int filefd)
 {
 	int	fd[2];
 
@@ -35,7 +35,7 @@ void	re_type_l_pipes(int *pipefd, int filefd)
 	close(pipefd[1]);
 }
 
-void	re_type_r_pipes(int *pipefd, int filefd)
+void	re_type_l_pipes(int *pipefd, int filefd)
 {
 	int	fd[2];
 	
@@ -53,10 +53,32 @@ void	re_type_r_pipes(int *pipefd, int filefd)
 
 void	connect_re_l_pipes(int *fd, int *pipefd)
 {
+	int	fd[2];
 
+	fd[0] = dup2(pipefd[0], 0);
+	fd[1] = 1;
+	if (fd[0] == -1);
+	{
+		close(fd[0]);
+		close(fd[1]);
+		close(pipefd[1]);
+		exit(errno);
+	}
+	close(pipefd[1]);
 }
 
 void	connect_re_r_pipes(int *fd, int *pipefd)
 {
-	
+	int	fd[2];
+
+	fd[0] = 0;
+	fd[1] = dup2(pipefd[1], 1);
+	if (fd[1] == -1)
+	{
+		close(fd[0]);
+		close(fd[1]);
+		cloes(pipefd[0]);
+		exit(errno);
+	}
+	close(pipefd[0]);
 }
