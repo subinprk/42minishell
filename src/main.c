362c5b1299ba41293a10c85f9453a99637d6c91f
Subin Park <subpark@c4a11c9.42berlin.de>
2023-12-05 15:52:47 +0100

    it works at tiny point...have to fix malloc errors

--- src/main.c
+++ src/main.c
@@ -6,13 +6,13 @@
 /*   By: subpark <subpark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 17:48:01 by subpark           #+#    #+#             */
-/*   Updated: 2023/12/05 14:17:48 by subpark          ###   ########.fr       */
+/*   Updated: 2023/12/05 15:50:40 by subpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int main()
+int main(int argc, char **argv, char **envs)
 {
 	char	**line;
 	int		index;
@@ -20,13 +20,13 @@ int main()
 
 	line = NULL;
 	index = 0;
-	g_envp = ft_split(getenv("envs"), '\n');
+	g_envp = cpy_full_2d_array(envs);
 	set_signal();
-	while(1)
+	while(argc && argv[0] != NULL)
 	{
 		get_line(line);
 		tree = extract_command(line[index]);
-		search_tree(tree, paths_array(g_envp));
+		search_tree(tree, paths_array(envs));
 		index ++;
 	}
 	exit(g_exit_status);
