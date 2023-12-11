449abd1523839da26dba1bd208fbeb43a75d48b3
Subin Park <subpark@c4a10c8.42berlin.de>
2023-12-11 15:32:29 +0100

    have to change about modifying node change

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: siun <siun@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: subpark <subpark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 17:48:01 by subpark           #+#    #+#             */
-/*   Updated: 2023/12/08 20:47:56 by siun             ###   ########.fr       */
+/*   Updated: 2023/12/11 15:20:47 by subpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,9 +36,9 @@ int main(int argc, char **argv, char **envs)
 		generate_prompt();
 		tmp = readline(" ");
 		tree = extract_command(tmp);
-	printf("%p\n", tree);
+	//printf("%p\n", tree);
 		//print_every_node(tree, 0);
-		//search_tree(tree, paths_array(envs));
+		search_tree(tree, paths_array(envs));
 		index ++;
 		//free_tree(tree);
 	}
--- src/parsing/syntax_analysis.c
+++ src/parsing/syntax_analysis.c
@@ -6,7 +6,7 @@
 /*   By: subpark <subpark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 03:40:29 by siun              #+#    #+#             */
-/*   Updated: 2023/12/08 19:13:34 by subpark          ###   ########.fr       */
+/*   Updated: 2023/12/11 15:31:42 by subpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 int	syntax_pipe(char **cmd_line, int *token, int *i, t_cmd *node)
 {
-	int	pipe_index;
-	int	tmp;
+	int		pipe_index;
+	int		tmp;
+	t_cmd	*head_tmp;
 
+	head_tmp = node;
 	tmp = i[1];
 	pipe_index = find_pipe(token, i);
 	node = generate_tree_node(NODE_CMD, pipe_index);
@@ -37,6 +39,7 @@ int	syntax_pipe(char **cmd_line, int *token, int *i, t_cmd *node)
 		i[1] = tmp;
 		return (syntax_pipe(cmd_line, token, i, node->right_child));
 	}
+	node = head_tmp;
 	return (1);
 }
 
@@ -44,7 +47,9 @@ int	syntax_cmds(char **cmd_line, int *token, int *i, t_cmd *node)
 {
 	int	redirect_index;
 	int	tmp;
+	t_cmd	*head_tmp;
 
+	head_tmp = node;
 	tmp = i[1];
 	node = generate_tree_node(NODE_REDIRECTS, -1);
 	redirect_index = find_redirection(token, i);
@@ -63,6 +68,7 @@ int	syntax_cmds(char **cmd_line, int *token, int *i, t_cmd *node)
 		i[1] = tmp;
 		return (syntax_redirects(cmd_line, token, i, node->right_child));
 	}
+	node = head_tmp;
 	return (1);
 }
 
@@ -70,20 +76,16 @@ int	syntax_simple_cmd(char **cmd_line, int *i, int *token, t_cmd *node)
 {
 	int		pipe_check[2];
 	int		pipe_e;
+	t_cmd	*head_tmp;
 
+	head_tmp = node;
 	pipe_check[0] = i[1];
 	pipe_check[1] = token_length(token);
-//printf("check pipecheck[1]: %d", pipe_check[1]);
 	pipe_e = find_pipe(token, pipe_check);
-//printf("start : %d    end: %d\n", i[0], i[1]);
 	node = generate_tree_node(NODE_SIMPLE_CMD, pipe_e);
 	node->left_child = generate_end_node(cmd_line, NODE_FILE_PATH, i[0], i[0] + 1);
 	node->right_child = generate_end_node(cmd_line, NODE_ARGV, i[0], i[1]);
-//printf("simple command pipe exist\n%d\n", pipe_e);
-//printf("\nsimple command left_child\n");
-//print_single_node(node->left_child);
-//printf("\nsimple command right_child\n");
-//print_single_node(node->right_child);
+	node = head_tmp;
 	return (1);
 }
 
@@ -91,13 +93,12 @@ int	syntax_redirects(char **cmd_line, int *token, int *i, t_cmd *node)
 {
 	int	next_redirect_index;
 	int	tmp;
+	t_cmd	*head_tmp;
 
-printf("redirection start : %d    end: %d\n", i[0], i[1]);
-printf("after redirection : %d\n", token[i[0]]);
-printf("end index print: %d\n", i[1]);
+	head_tmp = node;
+	node = generate_tree_node(NODE_REDIRECTS, -1);
 	tmp = i[1];
 	next_redirect_index = find_next_redirection(token, i);
-printf("next_redirect index : %d\n", next_redirect_index);
 	if (next_redirect_index != -1)
 		i[1] = next_redirect_index - 1;
 	if (syntax_simple_redirect(cmd_line, i, node->left_child) == -1)
@@ -108,20 +109,20 @@ printf("next_redirect index : %d\n", next_redirect_index);
 		i[1] = tmp;
 		return (syntax_redirects(cmd_line, token, i, node->right_child));
 	}
-printf("\nredirect left_child\n");
-print_single_node(node->left_child);
-printf("\nrecirect right_child\n");
-print_single_node(node->right_child);
+	node = head_tmp;
 	return (1);
 }
 
 int	syntax_simple_redirect(char **cmd_line, /*int *token,*/ int *i, t_cmd *node)
 {
-printf("entered here\n");
+	t_cmd	*head_tmp;
+
+	head_tmp = node;
 	node = generate_tree_node(NODE_SIMPLE_REDIRECT, -1);
 	node->left_child = generate_end_node(cmd_line, NODE_RED_TYPE,
 						i[0], i[0]);
 	node->right_child = generate_end_node(cmd_line, NODE_FILE_NAME,
 						i[0] + 1, i[1]);
+	node = head_tmp;
 	return (1);
 }
